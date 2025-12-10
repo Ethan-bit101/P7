aebce5bc1d5102f8a6c3be755ba20cc33411d237
Ethan <136395115+Ethan-bit101@users.noreply.github.com>
2025-12-10 21:12:57 +0700

    Update page.tsx

--- src/app/wasted/page.tsx
+++ src/app/wasted/page.tsx
@@ -26,7 +26,7 @@ import {
 import { Sheet,
          SheetClose,
          SheetContent,
-         SheetDesription,
+         SheetDescription,
          SheetFooter,
          SheetHeader,
          SheetTitle,
@@ -460,5 +460,6 @@ interface JwtPayload {
     </div>
   );
 }
+
 
 
