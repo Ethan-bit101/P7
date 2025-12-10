f9d630f8c8d9c82b0f3cbf2d17117484c7e09582
Ethan <136395115+Ethan-bit101@users.noreply.github.com>
2025-12-10 20:41:02 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- src/app/your_song/page.tsx
+++ src/app/your_song/page.tsx
@@ -326,9 +326,7 @@ interface JwtPayload {
                     <Link href="/profile" rel="noopener noreferrer">Profile</Link>
                   </DropdownMenuLabel>
 
-                  <DropdownMenuLabel>
-                    <Link href="/positions" rel="noopener noreferrer">Positions</Link>
-                  </DropdownMenuLabel>
+               
                   <DropdownMenuSeparator />
 
                   <DropdownMenuItem>
@@ -461,4 +459,5 @@ interface JwtPayload {
     </div>
   );
 }
+
 
